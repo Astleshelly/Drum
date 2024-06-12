3bb79fb0586fb2e9a48bbcb955d053d0483fc484
Astleshelly <astleshelly3@gmail.com>
2024-06-12 23:56:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ function click(val){
     switch (val) {
         
      case 'w':
-         a=new Audio('./sounds/crash.mp3');
+         a=new Audio('./crash.mp3');
          
          break;
      case 'a':
-         a=new Audio('./sounds/kick-bass.mp3');
+         a=new Audio('./kick-bass.mp3');
          break;
      case 's':
-         a=new Audio('./sounds/snare.mp3');
+         a=new Audio('./snare.mp3');
          break;
      case 'd':
-        var a=new Audio('./sounds/tom-1.mp3');
+        var a=new Audio('./tom-1.mp3');
          break;
      case 'j':
-         a=new Audio('./sounds/tom-2.mp3');
+         a=new Audio('./tom-2.mp3');
          break;
      case 'k':
-         a=new Audio('./sounds/tom-3.mp3');
+         a=new Audio('./tom-3.mp3');
          break;
      case 'l':
-         a=new Audio('./sounds/tom-4.mp3');
+         a=new Audio('./tom-4.mp3');
          break;    
      default:
  
